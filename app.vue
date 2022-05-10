586746fabbb27085d625c21f152204b967654b3b
linzhihui2022 <102964308+linzhihui2022@users.noreply.github.com>
2022-05-10 18:21:04 +0800

    feat: build config

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Hello,</h1>
+    <h1>Hello, welcome</h1>
     <h2>I am Lychee.</h2>
     <h2>Coming soon...</h2>
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  nitro: {
+    output: {
+      dir: "output",
+      serverDir: "output/server",
+      publicDir: "output/public",
+    },
+  },
+});
